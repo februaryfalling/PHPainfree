6968f7adf61acd8bdc80ac3b386b39094d56158d
Eric Ryan Harrison <me@ericharrison.info>
2010-05-13 15:53:07 +0000

    Separated config from Painfree.php.
    Didn't think to realize that "upgrading"
    would wipe out custom settings. This change
    will fix that.

--- includes/Painfree.php
+++ includes/Painfree.php
@@ -9,57 +9,19 @@ Author: Eric Ryan Harrison
 	
 Usage:
 	This script should be called by index.php in your document
-	root. You should define all of your configuration options
-	in this file.
+	root.
+
+	YOU MUST HAVE A FILE CALLED PainfreeConfig.php
+	in your includes/ directory. Define application
+	"configuration" in that file. You really don't
+	ever need to do anything with this file.
+	
+	Close your text editor now. This isn't the code you're
+	looking for.
 ****************************************************************/
 
-/*************************** Edit Me ***************************/
-$PainfreeConfig = array(
-	// ApplicationController is the primary controller for your
-	// application. Generic.php is provided, but doesn't do anything
-	// except look for the view in the templates/ folder
-	'ApplicationController' => 'Generic.php', 
-	
-	// BaseView is the name of your base template inside of the templates
-	// folder. This base view generally provides the overall framework
-	// of output for your application
-	'BaseView' => 'generic.tpl',
-	
-	// DefaultView is the default view
-	'DefaultView' => 'main',
-	
-	// DB Configuration
-	// each element in the array is a DB server to try. If one
-	// fails to connect, it will roll-over to the next
-	// leave the array empty for no DB connection
-	'Database' => array(),
-	/*
-	'Database' => array(
-		'PrimaryDB' => array(
-			'host'   => 'hostname.or.ip.of.server.1.com',
-			'user'   => 'username',
-			'pass'   => 'password',
-			'schema' => 'default_schema',
-			'port'   => 3306,
-		),
-		'BackupDB' => array(
-			'host'   => 'hostname.or.ip.of.server.2.com',
-			'user'   => 'username',
-			'pass'   => 'password',
-			'schema' => 'default_schema',
-			'port'   => 3306,
-		),
-	),
-	*/
-	
-	// hardcore configuration. only change this if you know what you're doing
-	'PathParameter'  => 'path',
-	'TemplateFolder' => 'templates/',
-	'LogicFolder'    => 'includes/',
-	
-);
+require 'PainfreeConfig.php'; // you must have this file
 
-/************************* Don't touch *************************/
 $Painfree = new PHPainfree($PainfreeConfig);
 include $Painfree->logic(); // load the application logic controller and process the request
 include $Painfree->view();  // load the view
