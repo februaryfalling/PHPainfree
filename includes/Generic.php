ec38d458bcbff17ae3b2d449a7f9bfd3b5507893
Ryan Laurie <iethree@gmail.com>
2022-03-03 07:52:25 -0600

    add api helper class

--- includes/Generic.php
+++ includes/Generic.php
@@ -1,18 +1,20 @@
 <?php
 /* This is a generic template for PHPainfree. It doesn't really do much */
 	$Generic = new Generic();
-	
+	include_once 'core/Api.php';
+
 	class Generic {
 		public function title() {
 			global $Painfree;
 			return 'PHPainfree Version ' . $Painfree->Version;
 		}
-	
+
 		public function __construct() {
 			global $Painfree;
-			
+
 			// $this->db = $Painfree->db;
 			$this->route = $Painfree->route;
+			$this->Api = new Api();
 		}
 	}
-	
+
