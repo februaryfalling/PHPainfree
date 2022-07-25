f30dbc2266390cfdabe936984883470f64f15cae
Eric Ryan Harrison <me@ericharrison.info>
2022-07-25 13:35:01 -0400

    Update index.php for modern installations

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,7 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . '../');
 
-	include 'includes/Painfree.php';
+// Uncomment if you're using Composer for PHP modules
+// require realpath('../vendor/autoload.php');
+
+include 'includes/Painfree.php';
